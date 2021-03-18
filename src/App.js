ae699b051dcb0603fd03a638fa9a4002db0a334e
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-18 12:43:38 -0700

    wrapped App in <ProductsContext.Provider>

--- src/App.js
+++ src/App.js
@@ -7,27 +7,46 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import { ProductsContext } from "./contexts/productContext";
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart(item);
 	};
 
 	return (
+		<ProductsContext.Provider value={{products, addItem}}>
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}	
+				<Route exact path="/">
+					<Products />
+				</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+
+		{/* <div className="App">
+			<Navigation cart={cart} />
+			
+			// Routes
+			<Route exact path="/">
+			<Products products={products} addItem={addItem} />
+			</Route>
+			
+			<Route path="/cart">
+			<ShoppingCart cart={cart} />
+			</Route>
+		</div> */}
 		</div>
+		</ProductsContext.Provider>
 	);
 }
 
